e73df357cd55b19b09ebfbb38d3a416c59e7833c
unknown <reinaldismail13@gmail.com>
2023-10-05 08:30:16 +0700

    updated kon kontol

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,6 +51,7 @@
                                 <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">JANCOK</h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                    KONTOL KON KABEH ASUUU
                                     Laravel has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
                                 </p>
                             </div>
